fc3535fb87d9484dde1c7197224ae48281ccb44b
VinCeProg <cananes.vincent.cueto@gmail.com>
2025-04-24 12:09:01 +0800

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+// selects the menu toggle button and menu
+const menuToggle = document.querySelector('.burger-toggle');
+const menu = document.querySelector('.menu');
+
+// listens to toggle
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active'); // toggles menu
+  navbar.classList.toggle('menu-active');
+  
+  // Change icon
+  if (menu.classList.contains('active')) {
+    menuToggle.innerHTML = '&#10005;'; // changes to x icon
+  } else {
+    menuToggle.innerHTML = '&#9776;'; // returns to menu icon
+  }
+});
+
+const btnSubmit = document.getElementById('submitButton');
+btnSubmit.addEventListener('click', () => {
+  alert("Feedback Submitted");
+  // add popover here
+});
